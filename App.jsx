23e8ecd5fae93e52ff70188955e44d2c8a54da17
HamzaAhmedKhan15 <ha475128@gmail.com>
2024-12-01 21:08:00 +0500

    First Build

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30, textAlign:'center'}}>Hamza Ahmed Khan</Text>
+      <Button title='Tap to Play'/>
+    </View>
+  )
+}
+
+export default App
